14fe3f249a4c88477f7d0c8fdb3174fefc21f011
scrubs767 <scrubs767@gmail.com>
2016-12-29 18:51:10 -0500

    Comments are displays markdown

--- VsTrello/UI/CardToolWindowControl.xaml.cs
+++ VsTrello/UI/CardToolWindowControl.xaml.cs
@@ -10,6 +10,7 @@ namespace VsTrello.UI
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     /// <summary>
     /// Interaction logic for CardToolWindowControl.
@@ -22,20 +23,7 @@ namespace VsTrello.UI
         public CardToolWindowControl()
         {
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "CardToolWindow");
+            CommandBindings.Add(new CommandBinding(NavigationCommands.GoToPage, (sender, e) => Process.Start((string)e.Parameter)));
         }
     }
 }
