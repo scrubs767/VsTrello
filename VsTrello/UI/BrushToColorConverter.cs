ebe96ab68a0122f8d6f103d3d366ea4d83e67b61
scrubs767 <scrubs767@gmail.com>
2016-12-31 21:58:01 -0500

    Fixed a scrolling issue for the markdown stuff

---
+++ VsTrello/UI/BrushToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VsTrello.UI
+{
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is System.Windows.Media.SolidColorBrush)
+            {
+                System.Windows.Media.SolidColorBrush brush = (System.Windows.Media.SolidColorBrush)value;
+                return brush.Color;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
